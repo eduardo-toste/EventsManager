cc195bcd513cbd0debf8d1e4ba90117984d56e8d
eduardo-toste <duciochetti@gmail.com>
2025-02-18 01:03:01 -0300

    criacao do controller de Event

---
+++ src/main/java/br/com/nlw/events/controller/EventController.java
@@ -0,0 +1,32 @@
+package br.com.nlw.events.controller;
+
+import br.com.nlw.events.model.Event;
+import br.com.nlw.events.service.EventService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+public class EventController {
+
+    @Autowired
+    private EventService service;
+
+    @PostMapping("/events")
+    public Event addNewEvent(@RequestBody Event newEvent){
+        return service.addNewEvent(newEvent);
+    }
+
+    @GetMapping("/events")
+    public List<Event> getAllEvents(){
+        return service.getAllEvents();
+    }
+
+    @GetMapping("/events/{prettyName}")
+    public ResponseEntity<Event> getByPrettyName(@PathVariable String prettyName){
+        Event evt = service.getByPrettyName(prettyName);
+        return (evt != null) ? ResponseEntity.ok().body(evt) : ResponseEntity.notFound().build();
+    }
+}
