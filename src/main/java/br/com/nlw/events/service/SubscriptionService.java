fe1d5cebe0c90d0b24e1c298a1e1f59ed1518896
eduardo-toste <duciochetti@gmail.com>
2025-02-18 18:33:07 -0300

    abrangendo os demais casos de uso

--- src/main/java/br/com/nlw/events/service/SubscriptionService.java
+++ src/main/java/br/com/nlw/events/service/SubscriptionService.java
@@ -1,5 +1,8 @@
 package br.com.nlw.events.service;
 
+import br.com.nlw.events.dto.SubscriptionResponse;
+import br.com.nlw.events.exception.EventNotFoundException;
+import br.com.nlw.events.exception.SubscriptionConflictException;
 import br.com.nlw.events.model.Event;
 import br.com.nlw.events.model.Subscription;
 import br.com.nlw.events.model.User;
@@ -21,20 +24,30 @@ public class SubscriptionService {
     @Autowired
     private SubscriptionRepo subscriptionRepo;
 
-    public Subscription createNewSubscription(String eventName, User user){
+    public SubscriptionResponse createNewSubscription(String eventName, User user){
         // Recuperar evento pelo nome
         Event evt =  eventRepo.findByPrettyName(eventName);
+
+        if(evt == null){ //Caso alternativo 2
+            throw new EventNotFoundException("Evento " + eventName + " não existe!");
+        }
         User userRec = userRepo.findByEmail(user.getEmail());
 
-        if(userRec == null){
-            userRec = userRepo.save(userRec);
+        if(userRec == null){ //Caso alternativo 1
+            userRec = userRepo.save(user);
         }
 
         Subscription subs = new Subscription();
         subs.setEvent(evt);
         subs.setSubscriber(userRec);
 
+        Subscription tmpSub = subscriptionRepo.findByEventAndSubscriber(evt, userRec);
+
+        if(tmpSub != null){
+            throw new SubscriptionConflictException("Já existe incrição para o usuário " + userRec.getName() + " no evento " + evt.getTitle());
+        }
+
         Subscription res = subscriptionRepo.save(subs);
-        return res;
+        return new SubscriptionResponse(res.getSubscriptionNumber(), "http://codecraft.com/subscription/"+res.getEvent().getPrettyName()+"/"+res.getSubscriber().getUserId());
     }
 }
