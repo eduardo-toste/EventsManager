5ce9541f5aacc74c99d37a8d6eda2562ff07c710
eduardo-toste <duciochetti@gmail.com>
2025-02-19 11:41:24 -0300

    criando ranking de indicacoes

--- src/main/java/br/com/nlw/events/repository/SubscriptionRepo.java
+++ src/main/java/br/com/nlw/events/repository/SubscriptionRepo.java
@@ -1,10 +1,27 @@
 package br.com.nlw.events.repository;
 
+import br.com.nlw.events.dto.SubscriptionRankingItem;
 import br.com.nlw.events.model.Event;
 import br.com.nlw.events.model.Subscription;
 import br.com.nlw.events.model.User;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface SubscriptionRepo extends CrudRepository<Subscription, Integer> {
     public Subscription findByEventAndSubscriber(Event event, User user);
+
+    @Query(value = "SELECT " +
+            "    COUNT(subscription_number) AS quantidade, " +
+            "    indication_user_id, " +
+            "    user_name " +
+            "FROM db_events.tbl_subscription as s " +
+            "INNER JOIN db_events.tbl_user as u ON s.indication_user_id = u.user_id " +
+            "WHERE indication_user_id IS NOT NULL " +
+            "AND event_id = :eventId " +
+            "GROUP BY indication_user_id " +
+            "ORDER BY quantidade DESC ", nativeQuery = true)
+    public List<SubscriptionRankingItem> generateRanking(@Param("eventId") Integer eventId);
 }
--- src/main/java/br/com/nlw/events/service/SubscriptionService.java
+++ src/main/java/br/com/nlw/events/service/SubscriptionService.java
@@ -1,5 +1,6 @@
 package br.com.nlw.events.service;
 
+import br.com.nlw.events.dto.SubscriptionRankingItem;
 import br.com.nlw.events.dto.SubscriptionResponse;
 import br.com.nlw.events.exception.EventNotFoundException;
 import br.com.nlw.events.exception.SubscriptionConflictException;
@@ -12,6 +13,8 @@ import br.com.nlw.events.repository.SubscriptionRepo;
 import br.com.nlw.events.repository.UserRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class SubscriptionService {
@@ -59,5 +62,15 @@ public class SubscriptionService {
 
         Subscription res = subscriptionRepo.save(subs);
         return new SubscriptionResponse(res.getSubscriptionNumber(), "http://codecraft.com/subscription/"+res.getEvent().getPrettyName()+"/"+res.getSubscriber().getUserId());
+    }
+
+    public List<SubscriptionRankingItem> getCompleteRanking(String prettyName){
+        Event evt = eventRepo.findByPrettyName(prettyName);
+
+        if(evt == null){
+            throw new EventNotFoundException("Ranking do evento " + evt.getTitle() + " não existe!");
+        }
+
+        return subscriptionRepo.generateRanking(evt.getEventId());
     }
 }
