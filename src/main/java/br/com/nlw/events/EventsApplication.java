7bfe759bfd64561d8a5ba3fa20af4c38dcbb0cca
eduardo-toste <duciochetti@gmail.com>
2025-02-19 17:09:25 -0300

    iniciando a documentação com swagger

--- src/main/java/br/com/nlw/events/EventsApplication.java
+++ src/main/java/br/com/nlw/events/EventsApplication.java
@@ -1,9 +1,12 @@
 package br.com.nlw.events;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@OpenAPIDefinition(info = @Info(title = "Events Manager", version = "1", description = "API desenvolvida para controlar eventos e suas inscrições"))
 public class EventsApplication {
 
 	public static void main(String[] args) {
--- src/main/java/br/com/nlw/events/controller/EventController.java
+++ src/main/java/br/com/nlw/events/controller/EventController.java
@@ -2,6 +2,12 @@ package br.com.nlw.events.controller;
 
 import br.com.nlw.events.model.Event;
 import br.com.nlw.events.service.EventService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.ExampleObject;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -9,21 +15,55 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
+@Tag(name = "Events")
 public class EventController {
 
     @Autowired
     private EventService service;
 
+    @Operation(summary = "Realiza a criação de um novo evento", method = "POST")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Evento criado com sucesso")
+    })
     @PostMapping("/events")
-    public Event addNewEvent(@RequestBody Event newEvent){
+    public Event addNewEvent(
+            @io.swagger.v3.oas.annotations.parameters.RequestBody(
+                    description = "Dados do evento a ser criado",
+                    required = true,
+                    content = @Content(
+                            mediaType = "application/json",
+                            examples = @ExampleObject(
+                                    name = "Exemplo de evento",
+                                    value = "{\n"
+                                            + "    \"title\": \"Nome Evento\",\n"
+                                            + "    \"location\": \"Online\",\n"
+                                            + "    \"price\": 0,\n"
+                                            + "    \"startDate\": \"2025-06-06\",\n"
+                                            + "    \"endDate\": \"2025-06-09\",\n"
+                                            + "    \"startTime\": \"19:00:00\",\n"
+                                            + "    \"endTime\": \"22:00:00\"\n"
+                                            + "}"
+                            )
+                    )
+            )
+            @RequestBody Event newEvent
+    ) {
         return service.addNewEvent(newEvent);
     }
 
+    @Operation(summary = "Realiza a busca de todos os eventos cadastrados", method = "GET")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Busca realizada com sucesso"),
+    })
     @GetMapping("/events")
     public List<Event> getAllEvents(){
         return service.getAllEvents();
     }
 
+    @Operation(summary = "Realiza a busca de um evento específico pelo seu Pretty Name", method = "GET")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Busca realizada com sucesso"),
+    })
     @GetMapping("/events/{prettyName}")
     public ResponseEntity<Event> getByPrettyName(@PathVariable String prettyName){
         Event evt = service.getByPrettyName(prettyName);
