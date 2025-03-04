d2f317df8420621cdbd44f0286be029390e7e98c
eduardo-toste <duciochetti@gmail.com>
2025-03-03 21:39:36 -0300

    ajustando date formatter

--- src/test/java/br/com/nlw/events/utils/DataHelper.java
+++ src/test/java/br/com/nlw/events/utils/DataHelper.java
@@ -2,23 +2,27 @@ package br.com.nlw.events.utils;
 
 import java.time.LocalDate;
 import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
 import java.util.HashMap;
 import java.util.Map;
 
 public class DataHelper {
     public static Map<String, Object> EventData(String title, String location,
-                                              int price, LocalDate startDate,
-                                              LocalDate endDate, LocalTime startTime,
-                                              LocalTime endTime) {
+                                                int price, LocalDate startDate,
+                                                LocalDate endDate, LocalTime startTime,
+                                                LocalTime endTime) {
+
+        DateTimeFormatter dateFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+        DateTimeFormatter timeFormatter = DateTimeFormatter.ofPattern("HH:mm:ss");
 
         Map<String, Object> eventData = new HashMap<>();
         eventData.put("title", title);
         eventData.put("location", location);
         eventData.put("price", price);
-        eventData.put("startDate", startDate);
-        eventData.put("endDate", endDate);
-        eventData.put("startTime", startTime);
-        eventData.put("endTime", endTime);
+        eventData.put("startDate", startDate.format(dateFormatter));
+        eventData.put("endDate", endDate.format(dateFormatter));
+        eventData.put("startTime", startTime.format(timeFormatter));
+        eventData.put("endTime", endTime.format(timeFormatter));
 
         return eventData;
     }
